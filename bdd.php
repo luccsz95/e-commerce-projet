89440c25d4ecdb72f36bd5a8565010445b980ff7
luccsz95 <111521099+luccsz95@users.noreply.github.com>
2025-01-28 15:57:37 +0100

    Vérifié que l'utilisateur à bien vérifié son email, et ne peux pas se connecter sans avoir valider son mail

--- bdd.php
+++ bdd.php
@@ -3,10 +3,5 @@ $servername = "localhost";
 $dbname = "e_commerce_project";
 $dbusername = "root";
 $dbpassword = "";
-
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
 ?>
 
