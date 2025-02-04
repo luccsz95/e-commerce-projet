f126e58ccb84a7905ba870c1fee09e7f18b951f6
isk452 <kouicicontact@yahoo.com>
2025-02-04 10:43:58 +0100

    navbar ok

--- navbar.js
+++ navbar.js
@@ -9,19 +9,19 @@ productInput.on('input', function() {
             url: 'recup_animals.php',
             type: 'GET',
             data: { search_term: query, timestamp: new Date().getTime() },
-            dataType: 'html', // Assure que la réponse est du HTML
+            dataType: 'html', 
             cache: false,
             success: function(response) {
                 console.log("Réponse AJAX :", response);
                 datalist.html(response);
             },
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.error('Erreur de la requête.');
-                console.error('Statut :', textStatus);
-                console.error('Erreur :', errorThrown);
-                console.error('Réponse serveur :', jqXHR.responseText);
-                datalist.html('<option value="">Erreur lors de la récupération des suggestions.</option>');
-            }
+            // error: function(jqXHR, textStatus, errorThrown) {
+            //     console.error('Erreur de la requête.');
+            //     console.error('Statut :', textStatus);
+            //     console.error('Erreur :', errorThrown);
+            //     console.error('Réponse serveur :', jqXHR.responseText);
+            //     datalist.html('<option value="">Erreur lors de la récupération des suggestions.</option>');
+            // }
         });
     } else {
         datalist.empty();
--- navbar.php
+++ navbar.php
@@ -1,3 +1,9 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -6,22 +12,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Navbar</title>
 
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <!-- jQuery -->
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-pzjw8f+ua7Kw1TIq0jGniDqzN9V0mp7erHKKK7jw8JAzXb6zE15j82le79Zp5gZo" crossorigin="anonymous"></script>
 
-    <!-- Script externe -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pzjw8f+ua7Kw1TIq0jGniDqzN9V0mp7erHKKK7jw8JAzXb6zE15j82le79Zp5gZo" crossorigin="anonymous"></script> -->
     <script src="navbar.js" defer></script>
+    <link rel="stylesheet" href="style/navbar.css">
 </head>
 
 <body>
+
+    <?php if (!isset($_SESSION['firstname'])): ?>
+        <a href="connexion.php">Se connecter</a>
+    <?php else: ?>
+        <span>Bienvenue, <?php echo htmlspecialchars($_SESSION['firstname']); ?></span>
+        <form action="logout.php" method="POST" style="display:inline;">
+            <button type="submit">Se déconnecter</button>
+        </form>
+    <?php endif; ?>
+
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Logo</a>
@@ -30,7 +44,7 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <form class="d-flex my-2 my-lg-0">
+                <form class="search-form" action="search.php">
                     <input list="products" name="product" id="product" class="form-control me-2"
                         placeholder="Rechercher un produit" style="width: 350px;">
                     <datalist id="products"></datalist>
