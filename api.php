1a0bfc3d189cb833f9dd971776522eef816e3f7f
luccsz95 <111521099+luccsz95@users.noreply.github.com>
2025-02-10 11:01:48 +0100

    stockage des valeurs de la base de donnée dans api.php

---
+++ api.php
@@ -0,0 +1,18 @@
+<?php
+include 'bdd.php';
+
+$servername = "localhost";
+$dbname = "e_commerce_project";
+$dbusername = "root";
+$dbpassword = "";
+
+$pdo = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
+
+$getTableDB = $pdo->query('show tables;')->fetchAll(PDO::FETCH_COLUMN);
+
+foreach ($getTableDB as $table) {
+    $sqlQuery = $pdo->query('select * from ' . $table);
+    $sqlstock = $sqlQuery->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($sqlstock);
+}
+?>
